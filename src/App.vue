fe0dcf09b42a1d74051dccbd3c557c0c5c4f2478
wiese <pablo@grass.digital>
2021-02-15 17:11:40 +0100

    obey by prettier
    Not sure how those were introduced - serve script linting certainly
    did not show them at the time…

--- src/App.vue
+++ src/App.vue
@@ -36,15 +36,17 @@
             {{ $i18n.t("preferences.noteReading.speed.title") }}
           </label>
           <select
-              id="jaslmus-preferences-noteReading-speed"
-              v-model="preferences.noteReading.speed"
+            id="jaslmus-preferences-noteReading-speed"
+            v-model="preferences.noteReading.speed"
           >
             <option
-                v-for="option in preferencesChoices.noteReading.speed"
-                v-bind:value="option.value"
-                v-bind:key="option.id"
+              v-for="option in preferencesChoices.noteReading.speed"
+              v-bind:value="option.value"
+              v-bind:key="option.id"
             >
-              {{ $i18n.t(`preferences.noteReading.speed.choices.${option.id}`) }}
+              {{
+                $i18n.t(`preferences.noteReading.speed.choices.${option.id}`)
+              }}
             </option>
           </select>
           <label for="jaslmus-preferences-noteReading-noteLimit">
