c138e2c1373f2ee3eb4c8b6c62281619dbb8a7fe
wiese <pablo@grass.digital>
2021-02-22 11:38:22 +0100

    challengeGenerator.spec: use more life-like base note

--- tests/unit/challengeGenerator.spec.ts
+++ tests/unit/challengeGenerator.spec.ts
@@ -2,13 +2,13 @@ import challengeGenerator from "@/challengeGenerator";
 
 describe("challengeGenerator", () => {
   it("can be constructed with a base note and note limit", () => {
-    const generator = challengeGenerator(5, 3);
+    const generator = challengeGenerator(60, 3);
 
     expect(generator.next).toBeInstanceOf(Function);
   });
 
   it("yields value between base note and note limit", () => {
-    const baseNote = 5,
+    const baseNote = 60,
       noteLimit = 3;
     const generator = challengeGenerator(baseNote, noteLimit);
 
@@ -20,7 +20,7 @@ describe("challengeGenerator", () => {
   });
 
   it("does not yield the same note twice in a row if it can help it (by generating another one)", () => {
-    const baseNote = 5;
+    const baseNote = 60;
     const generator = challengeGenerator(baseNote, 2);
 
     const mathRandom = jest.spyOn(Math, "random");
@@ -28,13 +28,13 @@ describe("challengeGenerator", () => {
       .mockReturnValueOnce(0.1)
       .mockReturnValueOnce(0.1)
       .mockReturnValueOnce(0.5);
-    expect(generator.next().value).toBe(5);
-    expect(generator.next().value).toBe(6);
+    expect(generator.next().value).toBe(60);
+    expect(generator.next().value).toBe(61);
     expect(mathRandom).toHaveBeenCalledTimes(3);
   });
 
   it("yields the same note time and again if it's the only one allowed", () => {
-    const baseNote = 5;
+    const baseNote = 60;
     const generator = challengeGenerator(baseNote, 1);
 
     expect(generator.next().value).toBe(baseNote);
